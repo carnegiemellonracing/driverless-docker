6e876269d7f9098bd24593a217eb9c3d62f4abd8
aryalohia <aryalohia00@gmail.com>
2025-07-21 01:59:55 -0400

    switched to docker compose system with seperate novnc service and 26x service. Updated docs to run container. gtsam + slam untested

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
 # driverless-docker
 
-Note: Windows users please use WSL (Windows Subsystem for Linux)
-
 ## Prerequisites:
-Docker
+install docker for your machine.
+[Docker desktop](https://docs.docker.com/desktop/) recommended, as it has multiple useful tools (e.g., GUI) + docker compose
 
 ## To clone the repository:
 `git clone https://github.com/carnegiemellonracing/driverless-docker.git`
 
-## To build the Docker container:
-`cd driverless-docker` <br>
-`docker pull ubuntu:22.04`
-`sudo docker build -t driverless-image .`
+## To build / run the Docker container:
+```bash
+cd driverless-docker
+docker compose up
+```
 
-## To run the Docker container:
-`sudo docker run -it driverless-image bash`
+## To run scripts from the container (attach terminal)
+```bash
+docker exec -it driverless-docker-26x-1 /bin/bash
+```
 
-## To run the Docker container with visualization (Linux):
-`sudo docker run -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" driverless-image bash`
+- Note: Would also run with dev-containers extension with vscode
 
-## To run the Docker container with visualization (Mac):
-1. Install XQuartz from https://www.xquartz.org
-2. `open -a XQuartz`
-3. Go to settings with `CTRL + ,` or from menu bar and ensure the Security tab looks as below
-![alt text]/securitySettings.jpg
-4. Restart your Mac
-5. `open -a XQuartz`
-6. xhost +localhost
-7. docker run -it -e DISPLAY=docker.for.mac.host.internal:0 driverless-image bash
+## To rebuild the Docker container:
+```bash 
+docker compose up --build
+```
+
+## Visualization
+open a tab in your browser of choice at localhost:8080
+Click on `vnc.html`
+
+- This opens an Ubuntu desktop
+- when you run commands with viz, such as `rviz2`, `rqt_graph`, `foxglove`, etc... it will show up in the browser tab
--- setup.sh
+++ setup.sh
@@ -1,34 +1,77 @@
 #!/bin/bash
 
+PATH_PLANNING=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -p|--pathplanning)
+            PATH_PLANNING=true
+            ;;
+        *)
+            echo "Unknown option: $1" >&2
+            exit 1
+            ;;
+    esac
+    shift # Consume the current flag
+done
+
 set -e
 
 apt update
 apt install -y git vim python3-pip locales curl tmux
 
-cd ~
-git clone https://github.com/carnegiemellonracing/PerceptionsLibrary22a.git
-cd PerceptionsLibrary22a
-pip install -r requirements24.txt
-export PYTHONPATH="$(pwd):$PYTHONPATH"
-echo "export PYTHONPATH=\"$(pwd):$PYTHONPATH\"" >> ~/.bashrc
-locale-gen en_US en_US.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
+#------------------------------------------------->
+#-----------------------OLD------------------------
+#<-------------------------------------------------
+# Perc 22a Library. Uncomment below lines to setup.
+# cd ~
+# git clone https://github.com/carnegiemellonracing/PerceptionsLibrary22a.git
+# cd PerceptionsLibrary22a
+# pip install -r requirements24.txt
+# export PYTHONPATH="$(pwd):$PYTHONPATH"
+# echo "export PYTHONPATH=\"$(pwd):$PYTHONPATH\"" >> ~/.bashrc
 
-apt install -y software-properties-common
-add-apt-repository universe -y
-apt update
-debconf-set-selections <<< "tzdata tzdata/Areas select America"
-debconf-set-selections <<< "tzdata tzdata/Zones/America select New_York"
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
-apt update
-apt install -y ros-humble-desktop
+
+# Below should be taken care of by using the ros humble desktop image.
+# locale-gen en_US en_US.UTF-8
+# update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+# export LANG=en_US.UTF-8
+
+# apt install -y software-properties-common
+# add-apt-repository universe -y
+# apt update
+# debconf-set-selections <<< "tzdata tzdata/Areas select America"
+# debconf-set-selections <<< "tzdata tzdata/Zones/America select New_York"
+# curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+# apt update
+# apt install -y ros-humble-desktop
+#------------------------------------------------->
+#----------------------END OLD---------------------
+#<-------------------------------------------------
+
 apt install -y ros-dev-tools ros-humble-tf-transformations libeigen3-dev libgsl-dev
 
-cd ~
+# Install GTSAM for Path Planning
+if $PATH_PLANNING; then 
+    cd ~
+    apt-get install -y libboost-all-dev cmake
+    git clone https://github.com/borglab/gtsam.git
+    cd gtsam
+    mkdir build
+    cd build
+    cmake ..
+    make install
+fi
+
+# Clone driverless repo
 git clone --recurse-submodules https://github.com/carnegiemellonracing/driverless.git
 cd driverless/driverless_ws
+
 source /opt/ros/humble/setup.bash
-colcon build --packages-up-to perceptions
-colcon build --packages-up-to planning
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "cd /root" >> ~/.bashrc
+
+# Add commands to build relevent packages
+# colcon build --packages-up-to perceptions_24a_cpp
+# colcon build --packages-up-to planning
