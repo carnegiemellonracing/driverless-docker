8dff8a1c01ad9dd75ec677ef4cc1a56cf204809d
Chris Su <chrisssu19@gmail.com>
2025-09-29 18:08:02 -0400

    Refactored controls sim docker to mount volumes manually instead

--- docker/controls-sim/spin_up.sh
+++ docker/controls-sim/spin_up.sh
@@ -18,7 +18,8 @@ xhost +local:root
 sudo docker run --rm -it \
   --gpus all \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v $DRIVERLESS_DOCKER/docker/controls-sim/canUsbKvaserTesting/linuxcan:/root/canUsbKvaserTesting/linuxcan \
+  -v $LIBS/canUsbKvaserTesting/linuxcan:/root/canUsbKvaserTesting/linuxcan \
+  -v $DRIVERLESS/:/root/driverless/ \
   -v $HOME/.Xauthority:/root/.Xauthority:ro \
   -e DISPLAY=$DISPLAY \
   -e XAUTHORITY=/root/.Xauthority \
