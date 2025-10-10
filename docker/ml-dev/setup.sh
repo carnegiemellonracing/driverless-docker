777d84917c2e59aa4c486e00c08f9e752c95c288
dale <dale@dale>
2025-10-09 20:20:44 -0400

    added git branch autocompletion in startup script

--- docker/ml-dev/setup.sh
+++ docker/ml-dev/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+apt install -y git gh vim python3-pip tmux curl
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~.git-completion.bash
+
 echo "Clone YOLO project repository"
 git clone https://github.com/carnegiemellonracing/driverless-ml-dev.git /root/driverless-ml-dev
+
 
 echo "ML Environment Setup Complete"
